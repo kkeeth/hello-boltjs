c9d0f2082aef6802ca8beb5404faccaad84c8f24
kkeeth <zensin0082@gmail.com>
2022-09-06 21:18:54 +0900

    feat: connection of messaging

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const { parsed } = require('dotenv').config()
 
 // ボットトークンと Signing Secret を使ってアプリを初期化します
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  token: parsed.SLACK_BOT_TOKEN,
+  signingSecret: parsed.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: parsed.SLACK_APP_TOKEN,
+  port: parsed.PORT || 3000
 })
 
+app.message('hello', async ({ message, say }) => {
+  // イベントがトリガーされたチャンネルに say() でメッセージを送信します
+  await say(`Hey there <@${message.user}>!`)
+})
 ;(async () => {
   // アプリを起動します
-  await app.start(process.env.PORT || 3000)
+  await app.start()
 
   console.log('⚡️ Bolt app is running!')
 })()
