d2942ac0694dea6cd643f616782c9aed3e08ed13
kkeeth <zensin0082@gmail.com>
2022-09-06 23:12:53 +0900

    feat: added button action

--- app.js
+++ app.js
@@ -12,8 +12,34 @@ const app = new App({
 
 app.message('hello', async ({ message, say }) => {
   // イベントがトリガーされたチャンネルに say() でメッセージを送信します
-  await say(`Hey there <@${message.user}>!`)
+  await say({
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hey there <@${message.user}>!`
+        },
+        accessory: {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: 'Click Me'
+          },
+          action_id: 'button_click'
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  })
 })
+
+app.action('button_click', async ({ body, ack, say }) => {
+  // アクションのリクエストを確認
+  await ack()
+  await say(`<@${body.user.id}> clicked the button`)
+})
+
 ;(async () => {
   // アプリを起動します
   await app.start()
